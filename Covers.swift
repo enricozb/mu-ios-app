35f9e88dfdd02c1c08fd4484b6fd297f91a3739f
Enrico Borba <enricozb@gmail.com>
2021-08-06 20:18:59 -1000

    initial caching

--- Covers.swift
+++ Covers.swift
@@ -1,20 +1,60 @@
 import Foundation
 import SwiftUI
 
-struct CoverCache {
+class URLImageCache {
   let cache = NSCache<NSString, UIImage>()
+  let mutex = DispatchQueue(label: "URLImageCache")
 
-  func get(forKey: String) -> UIImage? {
-    return cache.object(forKey: NSString(string: forKey))
+  var callbacks = [String: [(UIImage) -> Void]]()
+
+  func get(url: String, callback: @escaping (UIImage) -> Void) {
+    mutex.sync {
+      if let image = cache.object(forKey: NSString(string: url)) {
+        callback(image)
+        return
+      }
+
+      if callbacks[url] != nil {
+        callbacks[url] = callbacks[url]! + [callback]
+        return
+      }
+
+      callbacks[url] = [callback]
+
+      URLSession.shared.dataTask(
+        with: URL(string: url)!,
+        completionHandler: { data, _, error in
+          guard error == nil else {
+            log("handle image: error: \(error!)")
+            return
+          }
+          guard let data = data else {
+            log("handle image: no data")
+            return
+          }
+
+          DispatchQueue.main.async {
+            guard let image = UIImage(data: data) else { return }
+            self.publish(url: url, image: image)
+          }
+        }
+
+      ).resume()
+    }
   }
 
-  func set(forKey: String, image: UIImage) {
-    cache.setObject(image, forKey: NSString(string: forKey))
-  }
-}
+  func publish(url: String, image: UIImage) {
+    mutex.sync {
+      cache.setObject(image, forKey: NSString(string: url))
+      guard let urlCallbacks = callbacks[url] else { return }
 
-private extension CoverCache {
-  static let cache = CoverCache()
+      for callback in urlCallbacks {
+        callback(image)
+      }
+
+      callbacks.removeValue(forKey: url)
+    }
+  }
 }
 
 struct Cover: View {
@@ -34,34 +74,14 @@ struct Cover: View {
 }
 
 class URLImage: ObservableObject {
+  static var cache = URLImageCache()
+
   @Published var image: UIImage?
   let url: String
 
   init(url: String) {
     self.url = url
-    load()
-  }
-
-  func load() {
-    let task = URLSession.shared.dataTask(with: URL(string: url)!,
-                                          completionHandler: handleImage(data:response:error:))
-    task.resume()
-  }
-
-  func handleImage(data: Data?, response: URLResponse?, error: Error?) {
-    guard error == nil else {
-      log("handle image: error: \(error!)")
-      return
-    }
-    guard let data = data else {
-      log("handle image: no data")
-      return
-    }
-
-    DispatchQueue.main.async {
-      guard let image = UIImage(data: data) else {
-        return
-      }
+    URLImage.cache.get(url: url) { image in
       self.image = image
     }
   }
