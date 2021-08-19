bd52cafdda190fbb7927421ed7881e5941dd809f
Enrico Borba <enricozb@gmail.com>
2021-08-18 22:07:04 -1000

    WIP

--- MiniPlayer.swift
+++ MiniPlayer.swift
@@ -19,6 +19,37 @@ struct MiniPlayer: View {
 
   @EnvironmentObject var nowPlaying: NowPlaying
   @State private var showingSheet = false
+  @State private var height: CGFloat = MiniPlayer.Height
+  @State private var lerp: CGFloat = 0
+
+  @State private var albumScale: CGFloat = 1
+
+  var drag: some Gesture {
+    DragGesture()
+      .onChanged { drag in
+        self.height = MiniPlayer.Height + max(drag.startLocation.y - drag.location.y, 0)
+        self.lerp = min(450, self.height - MiniPlayer.Height) / 450
+
+        self.albumScale = 1 + 4 * self.lerp
+      }
+      .onEnded { drag in
+        if (drag.startLocation.y - drag.location.y) > 0 {
+          withAnimation {
+            self.height = 700
+            self.lerp = min(450, self.height - MiniPlayer.Height) / 450
+
+            self.albumScale = 1 + 4 * self.lerp
+          }
+        } else {
+          withAnimation {
+            self.height = MiniPlayer.Height
+            self.lerp = 0
+
+            self.albumScale = 1
+          }
+        }
+      }
+  }
 
   var body: some View {
     if let song = nowPlaying.song {
@@ -26,23 +57,34 @@ struct MiniPlayer: View {
         Spacer()
         Divider()
         HStack {
-          SongRow(song: song)
+          HStack {
+            Cover(album: song.album)
+              .frame(width: 50 * self.albumScale, height: 50 * self.albumScale)
+              .cornerRadius(3)
+            VStack(alignment: .leading) {
+              Text(song.title)
+                .lineLimit(1)
+              HStack {
+                Text("\(song.artist) · \(song.album)")
+                  .font(.caption)
+                  .foregroundColor(.gray)
+                  .lineLimit(1)
+              }
+            }
+            .opacity(Double(1 - self.lerp))
+          }
           Spacer()
           MiniPlayerButtons()
+            .opacity(Double(1 - self.lerp))
         }
-        .frame(height: MiniPlayer.Height)
+        .frame(height: height)
         .padding(.leading, 10)
-        .padding(.trailing, 10)
+        .padding(.trailing, 60 + self.albumScale * -50)
         .background(Blur(style: .systemChromeMaterial))
         Divider()
       }
       .padding(.bottom, 48)
-      .onTapGesture {
-        showingSheet.toggle()
-      }.sheet(isPresented: $showingSheet) {
-        NowPlayingDetail()
-          .accentColor(.purple)
-      }
+      .gesture(drag)
     } else {
       EmptyView()
     }
