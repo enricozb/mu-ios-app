f82662f56e1231711390d1abe7a3b09f509c660a
Enrico Borba <enricozb@gmail.com>
2021-08-27 11:20:55 -0400

    add AudioStreaming dep

---
+++ lib/AudioStreaming/Streaming/AudioPlayer/AudioPlayerContext.swift
@@ -0,0 +1,56 @@
+//
+//  Created by Dimitrios Chatzieleftheriou on 16/06/2020.
+//  Copyright © 2020 Decimal. All rights reserved.
+//
+
+import Foundation
+
+internal final class AudioPlayerContext {
+    var stopReason = Protected<AudioPlayerStopReason>(.none)
+
+    var state = Protected<AudioPlayerState>(.ready)
+    var stateChanged: ((_ oldState: AudioPlayerState, _ newState: AudioPlayerState) -> Void)?
+
+    var muted = Protected<Bool>(false)
+
+    var internalState: AudioPlayer.InternalState {
+        playerInternalState.value
+    }
+
+    let entriesLock = UnfairLock()
+    var audioReadingEntry: AudioEntry?
+    var audioPlayingEntry: AudioEntry?
+
+    var disposedRequested: Bool
+
+    /// This is the player's internal state to use
+    /// - NOTE: Do not use directly instead use the `internalState` to set and get the property
+    /// or the `setInternalState(to:when:)`method
+    private var playerInternalState = Protected<AudioPlayer.InternalState>(.initial)
+
+    init() {
+        disposedRequested = false
+    }
+
+    /// Sets the internal state if given the `inState` will be evaluated before assignment occurs.
+    /// This also convenvienlty sets the `stopReason` as well
+    /// - parameter state: The new `PlayerInternalState`
+    /// - parameter inState: If the `inState` expression is not nil, the internalState will be set if the evaluated expression is `true`
+    /// - NOTE: This sets the underlying `__playerInternalState` variable
+    internal func setInternalState(to state: AudioPlayer.InternalState,
+                                   when inState: ((AudioPlayer.InternalState) -> Bool)? = nil)
+    {
+        let newValues = playerStateAndStopReason(for: state)
+        stopReason.write { $0 = newValues.stopReason }
+        guard state != internalState else { return }
+        if let inState = inState, !inState(internalState) {
+            return
+        }
+        playerInternalState.write { $0 = state }
+        let previousPlayerState = self.state.value
+        if newValues.state != previousPlayerState {
+            self.state.write { $0 = newValues.state }
+            stateChanged?(previousPlayerState, newValues.state)
+        }
+    }
+}
